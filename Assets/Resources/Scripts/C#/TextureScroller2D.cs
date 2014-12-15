380870afb821c85d432a54840aea2965d6799ecd
Cyriaque Skrapits <cysoun@gmail.com>
2014-12-15 11:24:34 +0100

    Animated intro screen

---
+++ Assets/Resources/Scripts/C#/TextureScroller2D.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * TextureScroller2D
+ * 
+ * Author : Cyriaque Skrapits
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Utility class allowing to scroll a texture in a specified offset and speed.
+/// </summary>
+public class TextureScroller2D : MonoBehaviour
+{
+	public float scrollSpeedX;
+	public float scrollSpeedY;
+	public float tileSizeX;
+	public float tileSizeY;
+
+	private Vector3 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+	
+	void Update ()
+	{
+		float newPositionX = Mathf.Repeat (Time.time * scrollSpeedX, tileSizeX);
+		float newPositionY = Mathf.Repeat (Time.time * scrollSpeedY, tileSizeY);
+		transform.position = startPosition + Vector3.left * newPositionX + Vector3.down * newPositionY;
+	}
+}
--- Assets/Resources/Scripts/C#/s_start.cs
+++ Assets/Resources/Scripts/C#/s_start.cs
@@ -4,12 +4,35 @@ using System.Collections;
 public class s_start : MonoBehaviour
 {
 	
-	public static int i_level; 
+	public static int i_level;
 	public static bool b_end;
+	private static float top;
+	private static float left;
+	private static float width;
 	public GUISkin stylesheet;
+	public GameObject[] grounds;
+
+	private static Vector3 worldPoint;
+	private static float speedGrounds = 0.05f;
+	private static float speedMountains = 2f;
 
 	void Start ()
 	{
+		worldPoint = Camera.main.ViewportToWorldPoint (new Vector3 (1.0f, 1.0f));
+		top = worldPoint.y;
+		left = -worldPoint.x;
+		width = -left * 2.0f;
+
+		//Camera.main.aspect = 4 / 3f;
+		grounds = new GameObject[4];
+		grounds [0] = GameObject.Find ("ground0");
+		grounds [1] = GameObject.Find ("ground1");
+		grounds [2] = GameObject.Find ("ground2");
+		grounds [3] = GameObject.Find ("ground3");
+
+		for (int i = 0; i < grounds.Length; i++) {
+			grounds [i].transform.position = new Vector3 (i * Random.Range (5, 15), -top + (grounds [i].renderer.bounds.size.y / 2));
+		}
 	}
 
 	void Update ()
@@ -20,6 +43,16 @@ public class s_start : MonoBehaviour
 			AudioListener.pause = !AudioListener.pause;
 		if (Input.GetKey (KeyCode.Q))
 			Application.Quit ();
+
+		for (int i = 0; i < grounds.Length; i++) {
+			float x = grounds [i].transform.position.x - speedGrounds;
+			float y = grounds [i].transform.position.y;
+
+			if (x < left - 10)
+				x = -left + Random.Range (5, 15);
+
+			grounds [i].transform.position = new Vector3 (x, y, 0);
+		}
 	}
 	
 	void OnGUI ()
@@ -54,6 +87,6 @@ public class s_start : MonoBehaviour
 		
 		if (GUI.Button (new Rect (Screen.width / 2 + 110, Screen.height / 2 + 50, 130, 40), lng.t [4] + "\nDavid Arango\nFabio Balli\nJeremy Bouchard\nYannick Gervais\nFlorian Moncomble", "link")) 
 			Application.ExternalEval ("window.open('http://www.fibrosekystique.net','fibrosekystique.net','width=640,height=480,left=0,top=0')");
-		
+
 	}
 }
